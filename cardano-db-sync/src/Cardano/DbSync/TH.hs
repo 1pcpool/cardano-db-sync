94d2a0bc66ea886fb5c5090d667669af923bff3a
Tim McGilchrist <tim.mcgilchrist@iohk.io>
2021-09-29 17:15:23 +1000

    Fix reported git revisions in version command
    Place TH gitRev into cardano-db-sync project.
    
    Closes: https://github.com/input-output-hk/cardano-db-sync/issues/844

---
+++ cardano-db-sync/src/Cardano/DbSync/TH.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
+module Cardano.DbSync.TH
+  ( gitRevFromGit
+  ) where
+
+import           Cardano.Prelude
+
+import           Data.String (String)
+
+import qualified Language.Haskell.TH as TH
+
+import           System.IO.Error (IOError, ioeGetErrorType, isDoesNotExistErrorType)
+import           System.Process (readProcessWithExitCode)
+
+gitRevFromGit :: TH.Q TH.Exp
+gitRevFromGit =
+    TH.LitE . TH.StringL <$> TH.runIO runGitRevParse
+  where
+    runGitRevParse :: IO String
+    runGitRevParse = handleJust missingGit (const $ pure "") $ do
+        (exitCode, output, _) <- readProcessWithExitCode "git" ["rev-parse", "--verify", "HEAD"] ""
+        pure $ case exitCode of
+            ExitSuccess -> output
+            _otherwise -> ""
+
+    missingGit :: IOError -> Maybe ()
+    missingGit e = if isDoesNotExistErrorType (ioeGetErrorType e) then Just () else Nothing
